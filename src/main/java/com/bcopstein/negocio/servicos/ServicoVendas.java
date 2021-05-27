ed42775edc1af5f72c751c1741247c45bed4e925
Eduardo Soares <eduardoandre.mcc@gmail.com>
2021-05-27 03:56:18 +0000

    Factory funcionando + melhorias no code design no servico de vendas.

--- src/main/java/com/bcopstein/negocio/servicos/ServicoVendas.java
+++ src/main/java/com/bcopstein/negocio/servicos/ServicoVendas.java
@@ -8,7 +8,6 @@ import com.bcopstein.negocio.entidades.ItemVenda;
 import com.bcopstein.negocio.entidades.Produto;
 import com.bcopstein.negocio.entidades.Venda;
 import com.bcopstein.negocio.factory.LimiteVendaFactory;
-import com.bcopstein.negocio.repositorios.IEstoqueRepository;
 import com.bcopstein.negocio.repositorios.IVendaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,13 +35,14 @@ public class ServicoVendas {
     public boolean confirmaVenda(List<ItemEstoque> itens) {
 
         List<ItemEstoque> itensSolicitados = new ArrayList<ItemEstoque>();
+        List<ItemVenda> itemVendas = new ArrayList<ItemVenda>();
 
         for (ItemEstoque item : itens) {
-            
+
             Produto produto = servicoEstoque.buscarItemEstoqueByCodigo(item.getCodigo()).getProduto();
             ItemEstoque itemEstoque = new ItemEstoque(item.getCodigo(), item.getQuantidade(), produto);
             itensSolicitados.add(itemEstoque);
-            
+
         }
 
         if (limiteVendaFactory.getInstance().temRestricao(itensSolicitados)) {
@@ -51,16 +51,16 @@ public class ServicoVendas {
 
         int imposto = servicoEstoque.calculaSubtotal(itens)[1];
 
-        List<ItemVenda> itemVendas = new ArrayList<ItemVenda>();
-
         Venda venda = new Venda();
 
-        for (ItemEstoque item : itens) {
-            ItemEstoque itemEstoque = servicoEstoque.buscarItemEstoqueByCodigo(item.getCodigo());
+        for (int i = 0; i < itensSolicitados.size(); i++) {
 
-            ItemVenda itemVenda = new ItemVenda(item.getQuantidade(),
-                    itemEstoque.getProduto().getPrecoUnitario(), imposto, itemEstoque.getProduto(), venda);
+            ItemVenda itemVenda = new ItemVenda(itens.get(i).getQuantidade(),
+                    itensSolicitados.get(i).getProduto().getPrecoUnitario(), imposto,
+                    itensSolicitados.get(i).getProduto(), venda);
+
             itemVendas.add(itemVenda);
+
         }
 
         venda.setItemVenda(itemVendas);
